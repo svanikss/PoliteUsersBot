2943cc2a1871159b6274298b7ad5580e95c97abe
kooldawgstar <svanikss@gmail.com>
2016-08-23 10:12:12 -0400

    New Version of The Bot

--- politeusersbot.py
+++ politeusersbot.py
@@ -1,48 +1,29 @@
-#Polite Users Bot created by Kooldawgstar
-
 import praw
-from time import sleep
-import random
+import time
 
-USERNAME = "USERNAME"
-PASSWORD = "PASSWORD"
-LIMIT = 100
-RESPONSES = ["Thanks for being a nice user and thanking people for help!",
-             "Thank you for being a nice user and thanking people for help!",
-            ]
+r = praw.Reddit(user_agent = "A bot to thank users for being nice on reddit created by /u/kooldawgstar")
+print("Logging in...")
+r.login("USERNAME","PASSWORD", disable_warnings = True)
 
-responded = set()
+words_to_match = ['Please', 'please', 'You are welcome', 'May I', 'Excuse me', 'Pardon me', 'sorry', 'thanks']
+cache = []
 
-r = praw.Reddit(user_agent="Enter in Useragent here")
-r.login(USERNAME, PASSWORD)
-subreddit = r.get_subreddit("Polite_Users_Bot")
+nice = len(cache)
 
-def meets_criteria(responded, comment):
-    #add whatever criteria/logic you want here
-    return (not str(comment.author) == USERNAME) and (not comment.id in responded)  and ("thanks" , "thx" , "thank you" , "thank u" in comment.body.lower())
-
-def generate_response(comment):
-    #generate whatever response you want, you can make it specific to a comment by checking for various conditions
-    return random.choice(RESPONSES)
-
+def run_bot():
+    print("Grabbing subreddits...")
+    subreddit = r.get_subreddit("all")
+    comments = subreddit.get_comments(limit=25)
+    print("Grabbing comments...")
+    for comment in comments:
+        comment_text = comment.body.lower()
+        isMatch = any(string in comment_text for string in words_to_match)
+        if comment.id not in cache and isMatch:
+            print("Match found! Comment ID: " + comment.id)
+            comment.reply("Thank you for being a polite user on reddit! \n\n*This bot was created by [kooldawgstar](http://reddit.com/u/kooldawgstar), if this bot is an annoyance to your subreddit feel free to ban it or message me and I will add it to the blacklist.*")
+            print("Reply Sucessful")
+            cache.append(comment.id)
+    print("Comment loop finished, bot sleeping")
 while True:
-    for comment in subreddit.get_comments(limit=LIMIT):
-        if meets_criteria(responded, comment):
-            print (comment.body)
-            print (comment.id)
-            print (str(comment.author))
-            while True: #continue to try responding to the comment until it works, unless something unknown occurs
-                try:
-                    comment.reply(generate_response(comment))
-                    print ("Breaking out after responding, and adding to the list")
-                    responded.add(comment.id)
-                    break
-                except praw.errors.RateLimitExceeded:
-                    print ("Sleeping, rate limit :(")
-                    sleep(10*60) #sleep for 10 minutes, that's the timer limit
-                except:
-                    print ("Some unknown error has occurred, bail out...")
-                    break
-            print ("---------------------------------------\n\n")
-    print ("sleeping")
-    sleep(60) #sleep for a minute for new comments to show up
+    run_bot()
+    time.sleep(30)
